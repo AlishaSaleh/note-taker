bfdfb3059dd89bac1437d9d3abead55dfd2ecf54
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-04 13:15:45 +0000

    CSS and JS paths linked

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const path = require('path');
 const database = require('./db/db');
+const fs = require('fs');
 
 // Setting up Express
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 // Setting up Express to parse data
 app.use(express.urlencoded({ extended: true}));
@@ -16,12 +19,17 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.htm
 
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
+app.get('./public/assets/js/index.js', (req, res) => res.sendFile(path.join(__dirname, './public/assets/js/index.js')));
+app.get('./public/assets/css/styles.css', (req, res) => res.sendFile(path.join(__dirname, './public/assets/css/styles.css')));
+
 // API routes
 app.get('/api/notes', (req, res) => res.json(database));
 
 // Create new note 
 app.post('/api/notes', (req, res) => {
 // code for adding notes to db file here
+let newNote = req.body;
+
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
