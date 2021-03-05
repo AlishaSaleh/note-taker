569791f460fd12adffa43f7150479799ce68bfeb
Alisha Saleh <alishasaleh@hotmail.co.uk>
2021-03-05 17:16:04 +0000

    Comments added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const database = require('./db/db');
 const fs = require('fs');
 
-// global path for DB
+// Global path for Database
 const dbPath = path.join(__dirname, './db/db.json')
 
 // Setting up Express
@@ -28,41 +28,43 @@ app.get('./public/assets/css/styles.css', (req, res) => res.sendFile(path.join(_
 
 // API routes
 app.get('/api/notes', (req, res) => res.json(database));
-// app.get('/api/notes/:id', (req, res) => res.json(database[req.params.id]));
 
 // Create new note 
 app.post('/api/notes', (req, res) => {
-    // code for adding notes to db file here
     let newNote = req.body;
     let testID = 100;
+    // Code to add ID numbers on top the test ID of 100
     for (let i = 0; i < database.length; i++) {
         let note = database[i];
         if (note.id > testID) {
             testID = note.id;
         }
-
     };
-
     newNote.id = testID + 1;
-
+    // Pushes the new note to the database file
     database.push(newNote);
+    // Rewrites the database file
     fs.writeFile(dbPath, JSON.stringify(database), err =>
     err ? console.log(err) : console.log('Success! Note saved!'));
-
+    // Returns the note onto the page
     res.json(newNote);
 });
 
 app.delete('/api/notes/:id', (req, res) => {
     const noteID = req.params.id;
+    // Checks for the note to be deleted by ID
     for (let i = 0; i < database.length; i++) {
         if (database[i].id == noteID) { 
+            // Removes from database
             database.splice(i, 1);
             break;
         }
     };
+    // Rewrites database file
     fs.writeFile(dbPath, JSON.stringify(database), err =>
     err ? console.log(err) : console.log('Success! Note deleted!'));
-    return res.json(database);
+    // Returns to page
+    res.json(database);
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
